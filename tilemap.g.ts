a5704f28e2505b620af6002ac0f4369f44e4dec2
yuhdig <83087778+yuhdig@users.noreply.github.com>
2021-05-12 13:56:55 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,12 +3,35 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile1 = image.ofBuffer(hex``);
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101000000000001010101010001010101010000000000010100000001000001010100000000000101010101000101010001000000000000010101010101010101000100000000000001000001010000000001000000000000000101000001000000010000000000000000000101010101010101000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath6], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -26,6 +49,39 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1e001e0007131313121313131313131312131313131313131313121313131313130e061f1f1f1f1f1f1f1f0c1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f061f1f0a061f1f1f1f1f1f1f1f0c1f1f1f1f1f1f0d08081f1f1f1f1f1f1f061f1f0a111f1f1f1f1f1f1f0c1f1f1f1f1f1f1f101f1f081f1f1f1f1f1f061f1f0a061f1f1f1f1f1f1f0c1f1f1f1f1f1f1f0a1f1f1f08080808080808081f0a061f1f1f1f1f1f0c1f1f1f1f0d0808080b1f1f1f1f1f1f1f1f1f1f1f1f0a061f1f1f1f1f1f0c1f1f1f1f101f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f0a061f1f1f0d080b1f1f1f1f1f0a1f1f1f1f1f1f1f1f0d080812080808080a061f1f0a0a1f1f1f1f1f1f1f1f0a1f1f1f1f1f1f1f0a1f1f1f1f1f1f1f0a061f1f1f0a1f1f1f1f0d08091f1f0a1f1f1f1f1f0d0b1f1f1f1f1f1f1f0a061f1f1f1f1f1f1f0d0b1f05091f1f0a1f1f08080a1f1f0d0808091f1f0a111f1f1f1f1f1f1f0c1f1f1f061f1f0a1f1f1f1f0a1f1f0a1f1f061f1f0a061f1f1f1f1f1f1f0c1f1f1f061f1f1f0a1f1f1f0a1f1f0a1f1f111f1f10061f1f1f1f1f1f0d0b1f1f1f05091f1f0f0e1f1f0a1f1f0a1f1f061f1f0a061f1f1f1f1f1f101f0d08091f061f1f1f0a1f1f101f1f0a1f1f061f1f0a061f1f1f1f1f1f0c080b1f05091f1f1f1f0a1f1f0a1f1f0f130e061f1f0a111f1f1f1f1f1f0c1f1f1f1f061f1f1f1f0a1f1f0a1f1f1f1f0a061f1f0a061f1f1f1f1f1f0f0e1f1f1f061f1f1f0d0b1f1f0a1f1f1f1f0a061f1f0a061f1f1f1f1f1f1f0c1f1f1f050812080b1f1f1f0f0e1f1f1f0a061f1f0a061f1f1f1f1f1f1f0c1f1f1f1f1f1f1f1f1f1f1f1f0f0e1f1f0a061f1f10061f1f1f1f1f1f1f0c1f1f061f1f1f1f1f1f1f1f1f1f0a1f1f0a061f1f0a0508091f1f1f1f0d0b1f1f061f1f1f1f1f1f1f1f1f1f0a1f1f10061f1f0a1d1c061f1f1f1f0c1f1f1f05091f1f1f1f1f1f1f1f1f101f1f0a061f1f0a1c1d061f1f1f1f0c1f1f1f1f050808140808080808080b1f1f0a061f100a1d1c05080304080b1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f0a061f1f0a1c1d1c1d1a171e1c061f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f0a1f1f1f0a1d1c1d1c1a171c1e05080814080808080808080808140808080b061f1f021c1d1c1d1a1516181e1c1e1c1e1c1e1c1e1c1e1c1e1c1e1c1e1e061f1f011d1c1d1c191b1515161616161616161616161616161616161616050808081c1d1c1d1c1d191b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b`, img`
+222222222222222222222222222222
+2........2...................2
+2........2......222..........2
+2.......2.......2..2.........2
+2.......2.......2...22222222.2
+2......2....22222............2
+2......2....2................2
+2...222.....2........222222222
+2..22........2.......2.......2
+2...2....222..2.....22.......2
+2.......22.22..2..222..2222..2
+2.......2...2..2....2..2..2..2
+2.......2...2...2...2..2..2..2
+2......22...22..22..2..2..2..2
+2......2.222.2...2..2..2..2..2
+2......222.22....2..2..2222..2
+2......2....2....2..2....22..2
+2......22...2...22..2....22..2
+2.......2...22222...22...22..2
+2.......2............22..22..2
+2.......2..2..........2..22..2
+222....22..2..........2..22..2
+..2....2...22.........2..22..2
+..2....2....22222222222..22.22
+..22..22.................22..2
+........2................2...2
+........2222222222222222222...
+..........................2...
+..........................2222
+..............................
+`, [myTiles.transparency16,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.collectibleRedCrystal,myTiles.tile9,myTiles.tile11,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterSouth2,sprites.castle.tilePath5,sprites.castle.tilePath2,sprites.castle.tilePath6,sprites.castle.tilePath3,sprites.castle.tilePath7,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.dungeon.darkGroundCenter], TileScale.Sixteen);
         }
         return null;
     })
@@ -33,8 +89,12 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "myTile":
-            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile9":
+            case "tile11":return tile11;
         }
         return null;
     })
